a996b88b5c271af69c1c0f90f2bcc373fa79ac07
Dan Vatterott <dvatterott@gmail.com>
2018-06-02 20:19:11 -0500

    writing send_email to better handle the case when dates.txt is not present

--- send_email.py
+++ send_email.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     if path.isfile(file_path):
         with open(file_path) as f:
             dates = f.readlines()
-    dates = [x.rstrip('\n') for x in dates]
+    	dates = [x.rstrip('\n') for x in dates]
     
-    if any([x == dt.today().strftime(str_format) for x in dates]):
-        system("""echo Do X | mail -s "Reminder" -a "From: Dan <dan@example.com>" fake_addy@gmail.com""")
+    	if any([x == dt.today().strftime(str_format) for x in dates]):
+            system("""echo Do X | mail -s "Reminder" -a "From: Dan <dan@example.com>" fake_addy@gmail.com""")
     
